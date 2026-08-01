acceb110e57b23a30a7bad4677bb3a336145c78c
c-englert <claudio.englert@gmail.com>
2026-08-01 14:11:49 -0300

    Semana 05 form Take 3

--- scripts/avaliacao.js
+++ scripts/avaliacao.js
@@ -1,4 +1,3 @@
-// Controla o número de avaliações concluídas usando localStorage
 let reviewCount = Number(window.localStorage.getItem("reviewCount")) || 0;
 
 reviewCount += 1;
@@ -7,7 +6,6 @@ window.localStorage.setItem("reviewCount", reviewCount);
 
 document.querySelector("#review-count").textContent = reviewCount;
 
-// Rodapé
 document.querySelector("#year").textContent = new Date().getFullYear();
 document.querySelector("#lastModified").textContent =
   `Última modificação: ${document.lastModified}`;
